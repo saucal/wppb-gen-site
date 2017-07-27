42fc6ba6b8e710e737bd8aa6a26eeab9aae53f8e
Matias Saggiorato <matiassaggiorato@gmail.com>
2017-07-27 11:27:08 -0300

    Change port the app runs on.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 
 var express = require('express')
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3456;
 var path = require('path');
 var ghdownload = require('github-download');
 var rimraf = require('rimraf');
